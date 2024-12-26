9ccb279d971e88ec0c1c73fdc3d30af50f2835bd
4rjunc <arjunmundamani@gmail.com>
2024-12-26 18:44:26 +0530

    🚀 added query params, routes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
 use axum::{
-    response::Html,
-    routing::get,
-    Router,
+    extract::{Path, Query}, response::{Html, IntoResponse}, routing::get, Router
 };
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route(
-        "/test", 
-        get(|| async { Html( "Hello, <strong>World!</strong>") })
-    );
-
+    let app = Router::new().merge(routes_hello()) ;
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("->> LISTENING http://0.0.0.0:3000 \n");
     axum::serve(listener, app).await.unwrap();
+}
+
+//Routes Hello
+fn routes_hello() -> Router{
+        Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+// Handler Hello
+#[derive(Debug, Deserialize)]
+struct HelloParams{
+    name: Option<String>,
+}
+
+// e.g, `/hello?name=batman`
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse{
+    println!("->> {:<12} - handler_hello - {params:?}","HANDLER");
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello, <strong>{name}</strong>"))
+}
+
+// e.g, `/hello2/batman`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse{
+    println!("->> {:<12} - handler_hello2 - {name:?}","HANDLER");
+    Html(format!("Hello, <strong>{name}</strong>"))
+
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,6 +6,7 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()>{
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
-    hc.do_get("/test").await?.print().await?;
+    hc.do_get("/hello?name=batman").await?.print().await?;
+    hc.do_get("/hello2/batman").await?.print().await?;
     Ok(())
 }
